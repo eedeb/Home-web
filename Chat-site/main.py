396a63e72ef942202ebc3cea8e1020f029d9905c
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2024-04-28 20:13:48 -0400

    Update main.py

--- Chat-site/main.py
+++ Chat-site/main.py
@@ -16,7 +16,10 @@ def get_response():
         return jsonify({'response_1': 'reset', 'response_2': ''})
     else:
         responses = CustomChat.get_response(user_input, user_ip)  # Pass the user's IP address to get_response
+        if 'https://www.google.com/search?q' in responses[0]:
+            responses[0]=responses[0].split('Adequate answer not found. Open ')[1]
         return jsonify({'response_1': responses[0], 'response_2': responses[1]})
 
 if __name__ == '__main__':
     app.run(debug=True, host="172.235.133.234", port=80)
+
