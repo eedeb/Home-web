7dfed2c02b19a66c1b9413de4f8abd18454b88a4
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2024-09-24 10:16:30 -0400

    Update main.py

--- AI_page/main.py
+++ AI_page/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, render_template_string, make_response, send_from_directory
 import Classy
 import importlib
 
@@ -7,6 +7,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/api')
+def api_home():
+    return render_template('api.html')
+
+@app.route('/api/<path:text>')
+def api(text):
+    text = text.replace('%20', ' ')
+    x,y=Classy.classify(text,'/root/AI_page/static/data.pth')
+    output = x+", "+str(y)
+    return jsonify({"input": text,
+                    "output": x,
+                    "certainty": str(y)})
+
+
+
 
 @app.route('/info')
 def info():
@@ -14,18 +29,38 @@ def info():
 
 @app.route('/set_api_key', methods=['POST'])
 def set_api_key():
-    api_key = request.form['api_key']
-    importlib.reload(Classy.New_ai)
-    Classy.init('/root/AI_page/data.pth', api_key)
-    return jsonify({'status': 'success'})
+    api_key=request.form['api_key']
+    #importlib.reload(Classy.New_ai)
+    if request.cookies.get('key'):
+        Classy.init('/root/AI_page/static/data.pth', request.cookies.get('key'))
+        resp=make_response(jsonify({'status': 'success'}))
+        resp.set_cookie('key',request.cookies.get('key'))
+        api_key='cookie'
+    else:
+        api_key=request.form['api_key']
+    if api_key == 'cookie':
+        pass
+    elif api_key == 'Custom1':
+        Classy.init('/root/AI_page/static/data.pth','Custom API key 1')
+        resp=make_response(jsonify({'status': 'success'}))
+        resp.set_cookie('key','Custom API key 1')
+    elif api_key == 'Custom2':
+        Classy.init('/root/AI_page/static/data.pth','Custom API key 2')
+        resp=make_response(jsonify({'status': 'success'}))
+        resp.set_cookie('key','Custom API key 2')
+    else:
+        Classy.init('/root/AI_page/static/data.pth', api_key)
+        resp=make_response(jsonify({'status': 'success'}))
+        resp.set_cookie('key',api_key)
+    return resp
 
 @app.route('/get_response', methods=['POST'])
 def get_response():
+    cookie_key=request.cookies.get('key')
+    Classy.init('/root/AI_page/static/data.pth',cookie_key)
     user_input = request.form['user_input']
-    x, y, z, a = Classy.question(user_input)
-    if 'https://www.google.com/search?q' in x:
-        x = x.split('Adequate answer not found. Open ')[1]
+    x, y, z, a = Classy.server(user_input)
     return jsonify({'response_1': x, 'response_2': y})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="", port=80)
+    app.run(debug=False, ssl_context=('fullchain.pem','privkey.pem'))
